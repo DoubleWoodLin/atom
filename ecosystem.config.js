7cf2af8d987f703281ad60708766b2ef78fc1120
Jarvey <haojielin.linger@gmail.com>
2021-08-18 17:25:12 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
             user: 'root',
             host: '121.5.46.69',
             ref: 'origin/main',
-            repo: 'git@git.zhlh6.cn:Nick930826/atom-vite-h5.git',
+            repo: 'git@github.com:DoubleWoodLin/atom.git',
             path: '/atom-vite-h5',
             'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
             env: {
