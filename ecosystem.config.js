cfd9294cba9718eddf968ea1886a0c1345a19418
Jarvey <haojielin.linger@gmail.com>
2021-08-18 16:36:31 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
             host: '121.5.46.69',
             ref: 'origin/main',
             repo: 'git@git.zhlh6.cn:Nick930826/atom-vite-h5.git',
-            path: '/workspace/atom-vite-h5',
+            path: '/atom-vite-h5',
             'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
             env: {
                 NODE_ENV: 'production'
